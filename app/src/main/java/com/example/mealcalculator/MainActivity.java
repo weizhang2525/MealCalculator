057cea823e29f38d4408dc5b488d3ef6e136d158
Wei Zhang <wjzhang1@uci.edu>
2019-03-07 00:31:11 -0800

    Integrated Google Firebase into the application to allow users to read and write to the database. Added main screen to support database functionalities.

--- app/src/main/java/com/example/mealcalculator/MainActivity.java
+++ app/src/main/java/com/example/mealcalculator/MainActivity.java
@@ -8,55 +8,34 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private calculateTotal total = new calculateTotal();
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void calculateButton(View view)
+    public void addButton(View view)
     {
-        EditText textNum1 = findViewById(R.id.item1);
-        EditText textNum2 = findViewById(R.id.item2);
-        EditText textNum3 = findViewById(R.id.item3);
-        EditText textNum4 = findViewById(R.id.item4);
-        EditText textNum5 = findViewById(R.id.item5);
-        EditText textNum6 = findViewById(R.id.item6);
-        EditText textNum7 = findViewById(R.id.item7);
-        double entry1 = checkEmptyField(textNum1.getText().toString());
-        double entry2 = checkEmptyField(textNum2.getText().toString());
-        double entry3 = checkEmptyField(textNum3.getText().toString());
-        double entry4 = checkEmptyField(textNum4.getText().toString());
-        double entry5 = checkEmptyField(textNum5.getText().toString());
-        double entry6 = checkEmptyField(textNum6.getText().toString());
-        double entry7 = checkEmptyField(textNum7.getText().toString());
-        findSumPrice(entry1);
-        findSumPrice(entry2);
-        findSumPrice(entry3);
-        findSumPrice(entry4);
-        findSumPrice(entry5);
-        findSumPrice(entry6);
-        findSumPrice(entry7);
-        Bundle b = new Bundle();
-        b.putString("total", total.toString());
-        Intent intent = new Intent(this, calculateScreen.class);
-        intent.putExtras(b);
+        Intent intent = new Intent(this, addScreen.class);
         startActivity(intent);
     }
 
-    public double checkEmptyField(String entry)
+    public void searchButton(View view)
     {
-        if(entry.length() == 0)
-            return 0.0;
-        return Double.valueOf(entry);
+        Intent intent = new Intent(this, searchScreen.class);
+        startActivity(intent);
     }
 
-    public void findSumPrice(double entry)
+    public void deleteButton(View view)
     {
-        calculateTotal price = new calculateTotal(entry);
-        total.addPrice(price);
+        Intent intent = new Intent(this, deleteScreen.class);
+        startActivity(intent);
+    }
+
+    public void viewButton(View view)
+    {
+        Intent intent = new Intent(this, viewScreen.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/example/mealcalculator/addScreen.java
@@ -0,0 +1,13 @@
+package com.example.mealcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_screen);
+    }
+}
