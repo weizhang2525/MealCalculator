13391432a83e4d47370e82730076bb7322455088
tracett1 <traceyyttran@gmail.com>
2019-03-15 17:50:18 -0700

    Merge all files

---
+++ app/src/main/java/com/example/mealcalculator/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.mealcalculator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static FirebaseDatabase dataBase = FirebaseDatabase.getInstance();
+    public static DatabaseReference mealEntry;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+    }
+
+    public void addButton(View view)
+    {
+        Intent intent = new Intent(this, addScreen.class);
+        startActivity(intent);
+    }
+
+    public void searchButton(View view)
+    {
+        Intent intent = new Intent(this, searchScreen.class);
+        startActivity(intent);
+    }
+
+    public void deleteButton(View view)
+    {
+        Intent intent = new Intent(this, deleteScreen.class);
+        startActivity(intent);
+    }
+
+    public void viewButton(View view)
+    {
+        Intent intent = new Intent(this, viewScreen.class);
+        startActivity(intent);
+
+    }
+
+    public void calculateButton(View view){
+        Intent intent = new Intent(this, IntroMenu.class);
+        startActivity(intent);
+    }
+}
